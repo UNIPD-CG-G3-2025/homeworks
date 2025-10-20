cc4613ea5c1a275828d661a173bf5e014e16bf65
Filippo Tiberio <filippo.tiberio@studenti.unipd.it>
2025-10-20 16:40:15 +0200

    Initial H1 - Comments and Signatures

---
+++ H1/H1.R
@@ -0,0 +1,35 @@
+############################################################
+# Homework 1 - SNP Data Analysis
+# Course: Computational Genomics 2025/2026
+# Date: 20/10/2025
+#
+# Description:
+# This script implements three functions for SNP analysis:
+# 1. compute_MAF(SNPdata): computes Minor Allele Frequencies (MAF)
+# 2. HWE_test(SNPdata, pdf_file_name): performs Hardy-Weinberg Equilibrium test
+# 3. SNP_association_test(filepath, indCTRL, MAFth=0.01, HWEalpha=0.01):
+#    filters SNPs by MAF and HWE, performs association tests, and
+#    computes Benjamini–Hochberg corrected q-values.
+#
+# Group members:
+
+############################################################
+
+compute_MAF <- function(SNPdata) {
+  #BODY OF THE FUNCTION
+  
+  return(0)
+}
+
+HWE_test <- function(SNPdata, pdf_file_name) {
+  #BODY OF THE FUNCTION
+  
+  return(0)
+}
+
+
+SNP_association_test <- function(filepath, indCTRL, MAFth=0.01, HWEalpha=0.01) {
+  #BODY OF THE FUNCTION
+  
+  return(0)
+}
